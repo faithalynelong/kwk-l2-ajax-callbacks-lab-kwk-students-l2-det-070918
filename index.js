237fc65bf464e36755ee7f90854376f95e992ed0
faithalynelong <falong313@pccsk12.com>
2018-07-16 17:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $(document).ready(function (){
 });
 function(searchRepositories){
   var searchTerms = $('#searchTerms').val()
-  
+$.get(`https://api.github.com/search/repositories?q=${searchTerms}`)
   
 }
